8bfaa815489312f5d46a0d24ca99baf622e00452
github username <github email address>
2020-02-18 17:23:06 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
+    resp.write "Hello, World!"
 
+    resp.finish
+  end
 end
